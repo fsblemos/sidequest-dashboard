0fd46eda5672847dce89957e6d391666987c9d96
Fernanda <fernandasblemos@gmail.com>
2018-09-24 22:31:02 -0300

    remove unused css

--- src/web/App.vue
+++ src/web/App.vue
@@ -3,14 +3,3 @@
     <h1>Sidequest Dashboard</h1>
   </div>
 </template>
-
-<style>
-#dashboard {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
